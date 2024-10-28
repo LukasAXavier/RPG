7845f2316a9e2ecda77d40a48513258940986060
Lukas <lukas.andrade.xavier@gmail.com>
2024-10-27 21:18:21 -0300

    update

--- Jogo.java
+++ Jogo.java
@@ -2,7 +2,7 @@ import java.util.Random;
 import java.util.Scanner;
 import monstros.Monstro;
 import personagens.*;
-// lixo uhumano
+// se leu seu cu e meu
 // Classe principal que controla o jogo e a interação com o jogador
 public class Jogo {
 
