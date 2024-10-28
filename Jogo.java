0ea5d0047dbce8364aa2f08e4ba805dd567a0faf
Lukas <lukas.andrade.xavier@gmail.com>
2024-10-27 22:52:05 -0300

    up to date

--- Jogo.java
+++ Jogo.java
@@ -2,7 +2,7 @@ import java.util.Random;
 import java.util.Scanner;
 import monstros.Monstro;
 import personagens.*;
-// negger negger neggers nigga
+// negger negger neggers niggas
 // Classe principal que controla o jogo e a interação com o jogador
 public class Jogo {
 
